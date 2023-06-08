e4d05977696289ca76ae3f8d3229732d7e9396f4
Jet <zhenjielau99@gmail.com>
2023-06-08 17:30:03 +0800

    minor bug

--- rpc-server/handler.go
+++ rpc-server/handler.go
@@ -3,9 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"math/rand"
 	"strings"
 	"time"
-	"math/rand"
 
 	"github.com/TikTokTechImmersion/assignment_demo_2023/rpc-server/kitex_gen/rpc"
 )
@@ -14,15 +14,15 @@ import (
 type IMServiceImpl struct{}
 
 func (s *IMServiceImpl) Send(ctx context.Context, req *rpc.SendRequest) (*rpc.SendResponse, error) {
-	
+
 	if err := validateSendRequest(req); err != nil {
 		return nil, err
 	}
 
 	timestamp := time.Now().Unix()
 	message := &Message{
-		Message: req.Message.GetText(),
-		Sender: req.Message.GetSender(),
+		Message:   req.Message.GetText(),
+		Sender:    req.Message.GetSender(),
 		Timestamp: timestamp,
 	}
 
@@ -30,7 +30,7 @@ func (s *IMServiceImpl) Send(ctx context.Context, req *rpc.SendRequest) (*rpc.Se
 	if err != nil {
 		return nil, err
 	}
-	
+
 	err = rdb.SaveMessage(ctx, roomID, message)
 	if err != nil {
 		return nil, err
@@ -42,14 +42,14 @@ func (s *IMServiceImpl) Send(ctx context.Context, req *rpc.SendRequest) (*rpc.Se
 }
 
 func (s *IMServiceImpl) Pull(ctx context.Context, req *rpc.PullRequest) (*rpc.PullResponse, error) {
-	
+
 	roomID, err := getRoomID(req.GetChat())
 	if err != nil {
 		return nil, err
 	}
 
 	limit := int64(req.GetLimit())
-	if limit == 0{
+	if limit == 0 {
 		limit = 10
 	}
 	start := req.GetCursor()
@@ -59,21 +59,21 @@ func (s *IMServiceImpl) Pull(ctx context.Context, req *rpc.PullRequest) (*rpc.Pu
 	if err != nil {
 		return nil, err
 	}
-	
+
 	respMessages := make([]*rpc.Message, 0)
 	var counter int64 = 0
 	var nextCursor int64 = 0
 	hasMore := false
-	for _, msg : range messages {
+	for _, msg := range messages {
 		if counter+1 > limit {
 			hasMore = true
 			nextCursor = end
 			break
 		}
 		temp := &rpc.Message{
-			Chat: req.GetChat(),
-			Text: msg.Message,
-			Sender: msg.Sender,
+			Chat:     req.GetChat(),
+			Text:     msg.Message,
+			Sender:   msg.Sender,
 			SendTime: msg.Timestamp,
 		}
 		respMessages = append(respMessages, temp)
@@ -109,19 +109,19 @@ func validateSendRequest(Req *rpc.SendRequest) error {
 
 }
 
-func getRoomID(chat string) (string, error){
+func getRoomID(chat string) (string, error) {
 	var roomID string
 
 	lowercase := strings.ToLower(chat)
 	senders := strings.Split(lowercase, ":")
 	if len(senders) != 2 {
 		err := fmt.Errorf("invalid ChatID `%s`, should be in the format of user1:user2", chat)
-		retunr "", err
+		return "", err
 	}
 
 	sender1, sender2 := senders[0], senders[1]
 
-	if comp := strings.Compare(sender1, sender2); comp == 1{
+	if comp := strings.Compare(sender1, sender2); comp == 1 {
 		roomID = fmt.Sprintf("%s:%s", sender2, sender1)
 	} else {
 		roomID = fmt.Sprintf("%s:%s", sender1, sender2)
